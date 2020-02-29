f8741934edf9ed07fb7737ca7a1cc1ea5bf04777
Duke Lu <2315328598@qq.com>
2020-02-29 21:15:02 +0800

    inti

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,13 @@
   html, body, #app {
     height: 100%;
     width: 100%;
+    margin: 0;
+    padding: 0;
   }
   .main {
     height: 100%;
     width: 100%;
+    margin: 0;
+    padding: 0;
   }
 </style>
